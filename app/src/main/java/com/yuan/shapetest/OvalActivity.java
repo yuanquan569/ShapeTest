5fcae4f1ab1eb7c42c334b1380b6bc9a7d07e826
xhfHIT <3529203702@qq.com>
2019-06-21 00:37:19 +0800

    add oval shape activity

---
+++ app/src/main/java/com/yuan/shapetest/OvalActivity.java
@@ -0,0 +1,13 @@
+package com.yuan.shapetest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OvalActivity extends Activity {
+
+    @Override
+    protected void onCreate(@androidx.annotation.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oval);
+    }
+}
